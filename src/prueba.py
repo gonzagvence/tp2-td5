2d3ea0f365be5c40dd736e3da07ac2c285bce147
Gonza <ggarciavence@gmail.com>
2024-06-13 08:58:24 -0300

    Ej 4 y 5

--- src/prueba.py
+++ src/prueba.py
@@ -86,7 +86,11 @@ def main_modelado_circulacion(): # Modelado de la red como circulación
 
 def main_modelado_cost_min(): # Modelado de la red como costo mínimo
 	filename = "instances/toy_instance.json"
-	#filename = "instances/retiro-tigre-semana.json"
+	filename = "instances/retiro-tigre-semana.json"
+	#filename = "instances/ejercicio4_.json" #Coordinación
+	#filename = "instances/ejercicio4_3.json" #Importancia puntualidad con ej4_2
+	#filename = "instances/ejercicio4_4.json" #Comparativa modelo con manual
+	#filename = "instances/ejercicio4_5.json" #No funcional
 
 	with open(filename) as json_file: # Cargo data
 		data = json.load(json_file)
@@ -140,7 +144,7 @@ def main_modelado_cost_min(): # Modelado de la red como costo mínimo
 		temp = [] # Creo lista temporal
 		for place in data["services"][service]["stops"]: # Para cada servicio agrego amba información del nodo a la lista
 			temp.append(crear_tupla(place,(data["services"][service]["demand"][0]/data["rs_info"]['capacity'])))
-		G.add_edge(temp[0], temp[1], weight=0, capacity = data["rs_info"]['max_rs'], color='green') # Agrego arista c/ peso 0 y capacidad máxima definida en JSON
+		G.add_edge(temp[0], temp[1], weight=0, capacity = data["rs_info"]['max_rs']-(data["services"][service]["demand"][0]/data["rs_info"]['capacity']), color='green') # Agrego arista c/ peso 0 y capacidad máxima definida en JSON
 	#-------------------------------------
 
 	#----| Agregamos aristas de trasnoche |----
@@ -152,8 +156,99 @@ def main_modelado_cost_min(): # Modelado de la red como costo mínimo
 	nx.draw(G, with_labels=True, font_weight='bold', pos=pos, edge_color=edge_colors, connectionstyle='arc3, rad = 0.2') # Dibujo grafo
 	flowCost, flow_dict = nx.capacity_scaling(G, demand='demand', weight='weight') # Obtengo costo y flujos
 	print(flowCost)
-	#print(flow_dict)
+	print(retiro[-1])
+	print(tigre[-1])
+	print(flow_dict[retiro[-1]],flow_dict[tigre[-1]])
 	#plt.show() # Grafico
+
+
+
+def main_modelado_cost_min_restricciones(sede, restriccion): # Modelado de la red como costo mínimo
+	filename = "instances/toy_instance.json"
+	filename = "instances/retiro-tigre-semana.json"
+	#filename = "instances/ejercicio4_.json" #Coordinación
+	#filename = "instances/ejercicio4_3.json" #Importancia puntualidad con ej4_2
+	#filename = "instances/ejercicio4_4.json" #Comparativa modelo con manual
+	#filename = "instances/ejercicio4_5.json" #No funcional
+	#filename = "instances/ejercicio4_restriccion.json"
+
+	with open(filename) as json_file: # Cargo data
+		data = json.load(json_file)
+	retiro = [] # Guardo Nodos retiro
+	tigre = [] # Guardo Nodos tigre
+	nodos = [] # Guardo todos los nodos
+
+	#-----| Limpieza de datos |-----
+	for service in data["services"]: # Itero sobre la data para obtener listas de cada nodo que representa la llegada/salida de un servicio
+		for place in data["services"][service]["stops"]:
+			demand = (data["services"][service]["demand"][0]/data["rs_info"]['capacity']) #Defino cota inferior que será utilizada para modelar como costo minimo
+			nodos.append(crear_tupla(place, demand)) # Agregamos todos a nodos
+			if place['station'] == 'Retiro': # Separamos en dos listas 'retiro' y 'tigre'
+				retiro.append(crear_tupla(place, demand)) # Creo tupla (tipo inmutable) con caracteristicas del nodo
+			else:
+				tigre.append(crear_tupla(place, demand)) # Creo tupla (tipo inmutable) con caracteristicas del nodo
+	#------------------------------
+
+	retiro = ordenar_tuplas_por_numero(retiro)  # Ordeno nodos (Para crear aristas)
+	tigre = ordenar_tuplas_por_numero(tigre)  # Ordeno nodos (Para crear aristas)
+	pos = positions(retiro, tigre) # Defino posiciones (Unicamente para visualizar)
+
+	G = nx.DiGraph() # Creamos Grafo G
+
+	#----| Agregamos nodos al grafo, definiendo su demanda (imbalance) para realizar costo mínimo |----
+
+	for i in range(0,len(retiro)): # Itero sobre retiro
+		if(retiro[i][2]=='D'): # Si es D (Salida)
+			G.add_node(retiro[i],demand=(retiro[i][3])) # Agrego nodo con demanda positiva
+		else: # Caso contrario
+			G.add_node(retiro[i],demand=(-retiro[i][3])) # Agrego nodo con demanda negativa
+
+	for i in range(0,len(tigre)): # Itero sobre tigre
+		if(tigre[i][2]=='D'): # Si es D (Salida)
+			G.add_node(tigre[i],demand=(tigre[i][3])) # Agrego nodo con demanda positiva
+		else: # Caso contrario
+			G.add_node(tigre[i],demand=(-tigre[i][3])) # Agrego nodo con demanda negativa
+
+	#---------------------------------------------------------------------------------------------------
+
+	#----| Agregamos aristas de traspaso |----
+	for i in range(0,len(retiro)-1): # Itero sobre retiro
+		G.add_edge(retiro[i],retiro[i+1], weight = 0, capacity = 1e10, color='blue') # Agrego arista n -> n+1 c/ costo 0 y capacidad infinita
+
+	for i in range(0,len(tigre)-1): # Itero sobre tigre
+		G.add_edge(tigre[i],tigre[i+1], weight = 0, capacity = 1e10, color='blue') # Agrego arista n -> n+1 c/ costo 0 y capacidad infinita
+	#-----------------------------------------
+
+	#----| Agregamos aristas de tren |----
+	for service in data["services"]: # Itero sobre servicios
+		temp = [] # Creo lista temporal
+		for place in data["services"][service]["stops"]: # Para cada servicio agrego amba información del nodo a la lista
+			temp.append(crear_tupla(place,(data["services"][service]["demand"][0]/data["rs_info"]['capacity'])))
+		G.add_edge(temp[0], temp[1], weight=0, capacity = data["rs_info"]['max_rs']-(data["services"][service]["demand"][0]/data["rs_info"]['capacity']), color='green') # Agrego arista c/ peso 0 y capacidad máxima definida en JSON
+	#-------------------------------------
+
+	#----| Agregamos aristas de trasnoche |----
+	if(sede=='Retiro'):
+		G.add_edge(retiro[-1],retiro[0], weight = 1, capacity = restriccion, color='red') # Agrego individualmente arísta n->1 c/ peso 1 (modificable con JSON) y capacidad restringida
+		G.add_edge(tigre[-1],tigre[0], weight = 1, capacity = 1e10, color='red') # Agrego individualmente arísta n->1 c/ peso 1 (modificable con JSON) y capacidad infinita
+		G.add_edge(retiro[-1],tigre[0], weight = 1, capacity = 1e10, color='red')
+	else:
+		G.add_edge(retiro[-1],retiro[0], weight = 1, capacity = 1e10, color='red') # Agrego individualmente arísta n->1 c/ peso 1 (modificable con JSON) y capacidad infinita
+		G.add_edge(tigre[-1],tigre[0], weight = 1, capacity = restriccion, color='red') # Agrego individualmente arísta n->1 c/ peso 1 (modificable con JSON) y capacidad restringida
+		G.add_edge(tigre[-1],retiro[0], weight = 1, capacity = 1e10, color='red')
+	#------------------------------------------
+
+	edge_colors = [G[u][v]['color'] for u, v in G.edges()] # Obtengo lista de colores de arístas
+	nx.draw(G, with_labels=True, font_weight='bold', pos=pos, edge_color=edge_colors, connectionstyle='arc3, rad = 0.2') # Dibujo grafo
+	flowCost, flow_dict = nx.capacity_scaling(G, demand='demand', weight='weight') # Obtengo costo y flujos
+	print(flowCost)
+	print(flow_dict[retiro[-1]],flow_dict[tigre[-1]])
+	#plt.show() # Grafico
+	print(retiro)
+	print(tigre)
+
+
 if __name__ == "__main__":
-	main_modelado_cost_min()
-	main_modelado_circulacion()
+	#main_modelado_cost_min()
+	#main_modelado_circulacion()
+	main_modelado_cost_min_restricciones('Retiro',100)
